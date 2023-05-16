2911aaedf79230cc32a1d32c67af44a9b0bc01a2
Germiprogramer <llorentemunozgerman@gmail.com>
2023-05-16 23:22:49 +0200

    commit

--- classes.py
+++ classes.py
@@ -1,68 +1,68 @@
 import random
 import threading
 import time
-from queue import Queue
+
 
 class Agente(threading.Thread):
 
-    def __init__(self):
+    def __init__(self, q, s):
         threading.Thread.__init__(self)
         self.ingredientes = None
+        self.q = q
+        self.s = s
+
 
     def poner_ingredientes(self):
         self.ingrediente = random.choice([0, 1, 2])
         if self.ingrediente == 0:
                 self.ingredientes = ["papel", "tabaco"]
-                q.put(self.ingredientes)
-                s.acquire()
+                self.q.put(self.ingredientes)
+                self.s.acquire()
                 print('El agente pone el papel y el tabaco')
                 time.sleep(2)
         elif self.ingrediente == 1:
                 self.ingredientes = ["papel", "cerillas"]
                 print('El agente pone el papel y las cerillas')
-                q.put(self.ingredientes)
-                s.acquire()
+                self.q.put(self.ingredientes)
+                self.s.acquire()
                 time.sleep(2)
         elif self.ingrediente == 2:
                 self.ingredientes = ["tabaco", "cerillas"]
-                q.put(self.ingredientes)
-                s.acquire()
+                self.q.put(self.ingredientes)
+                self.s.acquire()
                 print('El agente pone el tabaco y las cerillas')
                 time.sleep(2)
 
     def run(self):
         while True:
-            if q.empty():
+            if self.q.empty():
                 self.poner_ingredientes()
             else:
                  print("El agente recoge los ingredientes")
-                 q.get()
-                 s.release()
+                 self.q.get()
+                 self.s.release()
                  time.sleep(2)
                  
 
             
 
 class Fumador(threading.Thread):
-    def __init__(self, nombre, ingrediente, agente):
+    def __init__(self, nombre, ingrediente, agente, q, s):
         threading.Thread.__init__(self)
         self.nombre = nombre
         self.ingrediente = ingrediente
         self.agente = agente
 
+
     def run(self):
         while True:
             if self.ingrediente not in self.agente.ingredientes:
-                q.get()
-                s.release()
+                self.q.get()
+                self.s.release()
                 print(self.nombre + ' fuma')
                 time.sleep(2)
-                q.put(self.agente.ingredientes)
-                s.acquire()
+                self.q.put(self.agente.ingredientes)
+                self.s.acquire()
                 
             else:
                 pass
-
-
-q = Queue(1)
-s = threading.Semaphore(0)
--- fumadores.py
+++ fumadores.py
@@ -1,11 +1,15 @@
 from classes import *
+from queue import Queue
 
 if __name__ == "__main__":
+    q = Queue(1)
+    s = threading.Semaphore(0)
 
-    agente = Agente()
-    fumador1 = Fumador('Fumador 1', 'papel', agente)
-    fumador2 = Fumador('Fumador 2', 'tabaco', agente)
-    fumador3 = Fumador('Fumador 3', 'cerillas', agente)
+
+    agente = Agente(q, s)
+    fumador1 = Fumador('Fumador 1', 'papel', agente, q, s)
+    fumador2 = Fumador('Fumador 2', 'tabaco', agente, q, s)
+    fumador3 = Fumador('Fumador 3', 'cerillas', agente, q, s)
 
     agente.start()
     fumador1.start()
